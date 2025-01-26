d583943e2386ab1283451a19dd0ac74fc4a038f8
PrakharIITB <210020093@iitb.ac.in>
2025-01-26 08:27:57 +0000

    changes to set up bunny upload server

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
+const cors = require('cors');
 require('dotenv').config();
 const express = require('express');
 const uploadModule = require('./api/upload-module');
 
 const app = express();
-
+app.use(cors({
+  credentials: true,  
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'AccessKey', 'st-auth-mode', 'rid', 'x-file-name', 'x-start-byte', 'x-end-byte', 'x-total-size']
+}));
 app.use(express.json());
 
-app.use('/upload', uploadModule);
+app.use('/api', uploadModule);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',  () => {
   console.log(`Server listening on port ${PORT}`);
 });
