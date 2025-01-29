49b1a9ea94ac289cd2ec83b51dd43ef652cfa2d5
PrakharIITB <210020093@iitb.ac.in>
2025-01-30 00:59:34 +0530

    resolved error while gettting hashtags

--- backend/utilities/insta_influencer_posts_rmq_publish.js
+++ backend/utilities/insta_influencer_posts_rmq_publish.js
@@ -1,60 +1,73 @@
-const amqp = require('amqplib');
-const db = require('../db/db');
-const QUEUE_NAME = 'insta_influencer_posts_queue';
-const {createConnection} = require('./RMQ');
+const amqp = require("amqplib");
+const db = require("../db/db");
+const QUEUE_NAME = "insta_influencer_posts_queue";
+const { createConnection } = require("./RMQ");
 
 async function fetchInstaUsers() {
-    try {
-        // Fetch insta_influencers from db for us only
-        const rows = await db('insta_users')
-            .select('user_id', 'username', 'followers_count')
-            .whereIn('country', ['United States', 'India', 'Canada'])
-            .andWhere('followers_count', '>', 10000);
-        // const rows = await db('insta_users').select('user_id', 'username', 'followers_count').where('country', 'United States');
-        return rows; 
-    } catch (error) {
-        console.error('Error fetching Insta Users:', error);
-        throw error;
-    }
+  try {
+    // Fetch insta_influencers from db for us only
+    const rows = await db("insta_users")
+      .select("user_id", "username", "followers_count")
+      .whereIn("country", ["United States", "India", "Canada"])
+      .andWhere("followers_count", ">", 10000);
+    // const rows = await db('insta_users').select('user_id', 'username', 'followers_count').where('country', 'United States');
+    return rows;
+  } catch (error) {
+    console.error("Error fetching Insta Users:", error);
+    throw error;
+  }
 }
 
-async function sendToQueue(user) {
-    try {
-        const connection = await createConnection();
-        const channel = await connection.createChannel();
-        await channel.assertQueue(QUEUE_NAME, { durable: true });
-        // Add tasks to the queue
-        // user = {user_id: 4544, username: 'rickyrampage', followers_count: 15739};
-        const {user_id, username, followers_count} = user; 
-        const [task] = await db('influencer_posts_tasks')
-                            .insert({user_id, username, status: 'pending', followers_count})
-                            .returning('task_id');
-        const messagePayload = {
-            task_id: task.task_id,
-        }
-        channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(messagePayload)));
-        console.log(` [x] Sent message to queue '${QUEUE_NAME}':`, messagePayload);
-
-        await channel.close();
-        await connection.close();
-    } catch (error) {
-        console.error('Error sending tasks to queue:', error);
-        throw error;
+async function sendToQueue(user, channel) {
+  try {
+    
+    // Add tasks to the queue
+    // user = {user_id: 4544, username: 'rickyrampage', followers_count: 15739};
+    console.log(user);
+    const { user_id, username, followers_count } = user;
+    if (user_id && username && followers_count) {
+      const [task] = await db("influencer_posts_tasks")
+        .insert({ user_id, username, status: "pending", followers_count })
+        .returning("task_id");
+      const messagePayload = {
+        task_id: task.task_id,
+      };
+      channel.sendToQueue(
+        QUEUE_NAME,
+        Buffer.from(JSON.stringify(messagePayload))
+      );
+      console.log(
+        ` [x] Sent message to queue '${QUEUE_NAME}':`,
+        messagePayload
+      );
     }
+    
+  } catch (error) {
+    console.error("Error sending tasks to queue:", error);
+    throw error;
+  }
 }
 
 async function main() {
-    try {
-        const users = await fetchInstaUsers();
-        // console.log(users.length);
-       for(const user of users){
-        await sendToQueue(users);
-       }
-    } catch (error) {
-        console.error('Error in main function:', error);
-    } finally {
-        await db.destroy(); // Close the Knex connection
+  try {
+    const connection = await createConnection();
+    const channel = await connection.createChannel();
+    await channel.assertQueue(QUEUE_NAME, { durable: true });
+
+    const users = await fetchInstaUsers();
+    // console.log(users.length);
+    for (var i=2; i<users.length; i++) {
+        const user = users[i];
+        await sendToQueue(user, channel);
     }
+
+    await channel.close();
+    await connection.close();
+  } catch (error) {
+    console.error("Error in main function:", error);
+  } finally {
+    await db.destroy(); // Close the Knex connection
+  }
 }
 
 main();
--- backend/workers/insta_influencer_posts/processTask.js
+++ backend/workers/insta_influencer_posts/processTask.js
@@ -1,7 +1,15 @@
 const logger = require("./logger");
-const {DB_template_post, DB_template_mentions} = require("../instagram/templates") 
-const {upload_to_bunny_cdn, saveCarouselMedia, DB_store_hashtags, DB_store_mentions} = require("../instagram/processTask")
-const db = require("../../db/db")
+const {
+  DB_template_post,
+  DB_template_mentions,
+} = require("../instagram/templates");
+const {
+  upload_to_bunny_cdn,
+  saveCarouselMedia,
+  DB_store_hashtags,
+  DB_store_mentions,
+} = require("../instagram/processTask");
+const db = require("../../db/db");
 const INSTA_RAPID_API_URL = process.env.INSTA_RAPID_API_URL;
 const INSTA_RAPID_API_KEY = process.env.INSTA_RAPID_API_KEY;
 
@@ -41,20 +49,22 @@ async function fetchPosts(username, post_limit = 1000, type) {
         }
         let data = await response.json();
         postList = postList.concat(data.data.items);
-        logger.info(`${postList.length}/${post_limit} ${type} fetched for ${username}`);
-        if(data.data.items.at(-1).taken_at < 1640975400){
+        logger.info(
+          `${postList.length}/${post_limit} ${type} fetched for ${username}`
+        );
+        if (data.data.items.at(-1).taken_at < 1640975400) {
           success = true;
           break;
         }
         if (data.pagination_token) {
           url = `${INSTA_RAPID_API_URL}/v1/posts?username_or_id_or_url=${username}&pagination_token=${data.pagination_token}`;
-        } else{
+        } else {
           success = true;
           break;
         }
       } catch (error) {
         console.log(error);
-        
+
         logger.warn(
           `🟡 Attempt ${attempt} failed for ${username} with error: ${error.message}`
         );
@@ -69,8 +79,7 @@ async function fetchPosts(username, post_limit = 1000, type) {
               error: error.message,
               message: `🔴 Error at getting posts for ${username} after ${maxRetries} attempts`,
             };
-          }
-          else{
+          } else {
             return {
               status: 500,
               success: true,
@@ -98,123 +107,210 @@ async function fetchPosts(username, post_limit = 1000, type) {
   };
 }
 
-async function DB_store_mapping(post_id, user_id, movie_id, platform, trx){
-    try {
-        await trx('post_influencer_movie_map').insert({post_id, user_id, movie_id, platform})
-    } catch (error) {
-        logger.error("Error is storing mapping: ", error);
-    }
+async function DB_store_mapping(post_id, user_id, movie_id, platform, trx) {
+  try {
+    await trx("post_influencer_movie_map").insert({
+      post_id,
+      user_id,
+      movie_id,
+      platform,
+    });
+  } catch (error) {
+    logger.error("Error is storing mapping: ", error);
+  }
 }
 
 async function DB_store_post(trx, postData, task_id, user_id, followers_count) {
-    /**
-     * Stores the post in the database.
-     * If the post already exists in the database, it is updated.
-     * If the post doesn't exist in the database, it is inserted. 
-    **/
-    try {
-        // Getting UserID from Insta User ID
-        const insta_user_id = postData['user']['id'];
-        
-        const template = DB_template_post(postData, null, null, followers_count);
-        // checking if the post exists
-        let post_id = null;
-        
-        // If post doesn't exist, insert a new record with null values and get the new post_id
-        const existingPost = (await trx('insta_posts').select('post_id').where('insta_post_id', postData['id']).first());
-        
-        if (!existingPost) {
-            post_id = (await trx('insta_posts').insert(template).returning('post_id'))[0].post_id;
-            await trx('insta_posts').update({taskids: `"${task_id}"`}).where('post_id', post_id);
-        }else{
-            const taskids = (await trx('insta_posts').select('taskids').where('post_id', existingPost.post_id).first()).taskids;
-            let taskidsArray = taskids ? taskids.split(',').map(id => id.trim().replace(/['"]+/g, '')) : [];
-            taskidsArray.push(task_id.toString());
-            const newTaskids = taskidsArray.map(id => `"${id}"`).join(', ');
-            await trx('insta_posts').update({taskids: newTaskids}).where('post_id', existingPost.post_id);
-            post_id = (await trx('insta_posts').update(template).where('post_id', existingPost.post_id).returning('post_id'))[0].post_id;
-        }
+  /**
+   * Stores the post in the database.
+   * If the post already exists in the database, it is updated.
+   * If the post doesn't exist in the database, it is inserted.
+   **/
+  try {
+    // Getting UserID from Insta User ID
+    const insta_user_id = postData["user"]["id"];
 
-        if (postData['media_name'] == 'album') {
-            await saveCarouselMedia(postData['carousel_media'], post_id, trx);    
-        }
-        else{
-            const bunnyCDNURL = (await upload_to_bunny_cdn(template['thumbnail_img'], 'prakhar/instagram', post_id, 'post')).image_url;
-            await trx('insta_posts').update({thumbnail_img: bunnyCDNURL}).where('post_id', post_id);
-            if(template['video_url']){
-                const videoBunnyCDNURL = (await upload_to_bunny_cdn(template['video_url'], 'prakhar/instagram', post_id, 'video')).image_url;
-                await trx('insta_posts').update({video_url: videoBunnyCDNURL}).where('post_id', post_id);
-            }
-        }
-        if (template['music_cover_img'] != null) {
-            const bunnyCDNURLforMusic = (await upload_to_bunny_cdn(template['music_cover_img'], 'prakhar/instagram', post_id, 'music')).image_url;
-            console.log(bunnyCDNURLforMusic);
-            if (bunnyCDNURLforMusic == null) {
-                logger.warn(`🟡 Error in uploading music cover image for post ${post_id}`);
-            }
-            if (bunnyCDNURLforMusic != null){
-                await trx('insta_posts').update({music_cover_img: bunnyCDNURLforMusic}).where('post_id', post_id);
-            }
-        }
-        
-        //store influencer and post mapping
-        await DB_store_mapping(post_id, user_id, null, "instagram", trx)
-        // Using POST_ID , storing the mentions and hashtags
-        
-        const hashtags = postData['caption']['hashtags'].map((hashtag) => (hashtag.split('#')[1]));
-        const mentions = postData['caption']['mentions'];
-        const tagged_users = postData['tagged_users'].map((user) => ({ username: user.user.username, id: user.user.id }));        
+    const template = DB_template_post(postData, null, null, followers_count);
+    // checking if the post exists
+    let post_id = null;
 
-        // Storing hashtags
-        if (hashtags) {
-            await DB_store_hashtags(trx, hashtags, post_id);
-        }
-        
-        // Storing Mentions
-        if (mentions || tagged_users) {
-            const all_mentions = DB_template_mentions(mentions, tagged_users, post_id);
-            await DB_store_mentions(trx, all_mentions, post_id);
-        }
-        return {status: 200, success: true,message: "Post Stored Successfully"};
+    // If post doesn't exist, insert a new record with null values and get the new post_id
+    const existingPost = await trx("insta_posts")
+      .select("post_id")
+      .where("insta_post_id", postData["id"])
+      .first();
 
-    } catch (error) {
-        return { status: 500, success: false, message: error.message };
+    if (!existingPost) {
+      post_id = (
+        await trx("insta_posts").insert(template).returning("post_id")
+      )[0].post_id;
+      await trx("insta_posts")
+        .update({ taskids: `"${task_id}"` })
+        .where("post_id", post_id);
+    } else {
+      const taskids = (
+        await trx("insta_posts")
+          .select("taskids")
+          .where("post_id", existingPost.post_id)
+          .first()
+      ).taskids;
+      let taskidsArray = taskids
+        ? taskids.split(",").map((id) => id.trim().replace(/['"]+/g, ""))
+        : [];
+      taskidsArray.push(task_id.toString());
+      const newTaskids = taskidsArray.map((id) => `"${id}"`).join(", ");
+      await trx("insta_posts")
+        .update({ taskids: newTaskids })
+        .where("post_id", existingPost.post_id);
+      post_id = (
+        await trx("insta_posts")
+          .update(template)
+          .where("post_id", existingPost.post_id)
+          .returning("post_id")
+      )[0].post_id;
     }
+
+    if (postData["media_name"] == "album") {
+      await saveCarouselMedia(postData["carousel_media"], post_id, trx);
+    } else {
+      const bunnyCDNURL = (
+        await upload_to_bunny_cdn(
+          template["thumbnail_img"],
+          "prakhar/instagram",
+          post_id,
+          "post"
+        )
+      ).image_url;
+      await trx("insta_posts")
+        .update({ thumbnail_img: bunnyCDNURL })
+        .where("post_id", post_id);
+      if (template["video_url"]) {
+        const videoBunnyCDNURL = (
+          await upload_to_bunny_cdn(
+            template["video_url"],
+            "prakhar/instagram",
+            post_id,
+            "video"
+          )
+        ).image_url;
+        await trx("insta_posts")
+          .update({ video_url: videoBunnyCDNURL })
+          .where("post_id", post_id);
+      }
+    }
+    if (template["music_cover_img"] != null) {
+      const bunnyCDNURLforMusic = (
+        await upload_to_bunny_cdn(
+          template["music_cover_img"],
+          "prakhar/instagram",
+          post_id,
+          "music"
+        )
+      ).image_url;
+      console.log(bunnyCDNURLforMusic);
+      if (bunnyCDNURLforMusic == null) {
+        logger.warn(
+          `🟡 Error in uploading music cover image for post ${post_id}`
+        );
+      }
+      if (bunnyCDNURLforMusic != null) {
+        await trx("insta_posts")
+          .update({ music_cover_img: bunnyCDNURLforMusic })
+          .where("post_id", post_id);
+      }
+    }
+
+    //store influencer and post mapping
+    await DB_store_mapping(post_id, user_id, null, "instagram", trx);
+    // Using POST_ID , storing the mentions and hashtags
+    const hashtags = postData.caption_hashtag
+      ? postData.caption_hashtag
+      : postData.caption?.hashtags || [];
+
+    const mentions = postData.caption_mention
+      ? postData.caption_mention
+      : postData.caption?.mentions || [];
+      
+    const tagged_users = postData.tagged_users?.map((user) => ({
+      username: user.user.username,
+      id: user.user.id,
+    }));
+
+    // Storing hashtags
+    if (hashtags.length > 0) {
+      await DB_store_hashtags(trx, hashtags, post_id);
+    }
+
+    // Storing Mentions
+    if (mentions || tagged_users) {
+      const all_mentions = DB_template_mentions(
+        mentions,
+        tagged_users,
+        post_id
+      );
+      await DB_store_mentions(trx, all_mentions, post_id);
+    }
+    return { status: 200, success: true, message: "Post Stored Successfully" };
+  } catch (error) {
+    return { status: 500, success: false, message: error.message };
+  }
 }
 
-async function savePosts(postsData, user_id, task_id, followers_count){
-    const trx = await db.transaction(); 
-    try {
-        let i = 1;
-        for (const post of postsData) {
-            const DB_response = await DB_store_post(trx, post, task_id, user_id, followers_count);
-            if (!DB_response.success) {
-                await trx.rollback();
-                logger.error(`🔴 Error storing Post at savePosts(task_id = ${task_id}, postid=${post.id}) | ${DB_response.message}`);
-                return {status: 500, success: false, message: DB_response.message};
-            }
-            if(i%20 == 0){
-              logger.info(`⚪ Successfully Stored ${i}/${postsData.length} posts(task_id = ${task_id})`);
-            }
-            i++;
-        }
-        await trx.commit();
-        logger.info(`⚪ Success at savePosts(task_id = ${task_id}) | Data Stored Successfully`);
-        return {status: 200, success: true};
-    } catch (error) {
+async function savePosts(postsData, user_id, task_id, followers_count) {
+  const trx = await db.transaction();
+  try {
+    let i = 1;
+    for (const post of postsData) {
+      const DB_response = await DB_store_post(
+        trx,
+        post,
+        task_id,
+        user_id,
+        followers_count
+      );
+      if (!DB_response.success) {
         await trx.rollback();
-        logger.error(`🔴 Error at savePosts(task_id = ${task_id}) | ${error.message}`);
-        return {status: 500, success: false, message: error.message};
+        logger.error(
+          `🔴 Error storing Post at savePosts(task_id = ${task_id}, postid=${post.id}) | ${DB_response.message}`
+        );
+        return { status: 500, success: false, message: DB_response.message };
+      }
+      if (i % 20 == 0) {
+        logger.info(
+          `⚪ Successfully Stored ${i}/${postsData.length} posts(task_id = ${task_id})`
+        );
+      }
+      i++;
     }
+    await trx.commit();
+    logger.info(
+      `⚪ Success at savePosts(task_id = ${task_id}) | Data Stored Successfully`
+    );
+    return { status: 200, success: true };
+  } catch (error) {
+    await trx.rollback();
+    logger.error(
+      `🔴 Error at savePosts(task_id = ${task_id}) | ${error.message}`
+    );
+    return { status: 500, success: false, message: error.message };
+  }
 }
 
-async function processTask(task_id, user_id, username, followers_count, post_limit) {
+async function processTask(
+  task_id,
+  user_id,
+  username,
+  followers_count,
+  post_limit
+) {
   try {
     // Fetch the user's posts
     const postFetchResponse = await fetchPosts(username, post_limit, "posts");
-    if(postFetchResponse.status != 200){
-        logger.info(`🔴 Error in fetching posts for ${username}, only ${postFetchResponse.data.length} posts fetched`);
-        return { status: 500, success: false}; 
+    if (postFetchResponse.status != 200) {
+      logger.info(
+        `🔴 Error in fetching posts for ${username}, only ${postFetchResponse.data.length} posts fetched`
+      );
+      return { status: 500, success: false };
     }
     const postData = postFetchResponse.data;
 
@@ -222,14 +318,19 @@ async function processTask(task_id, user_id, username, followers_count, post_lim
     // const reelFetchResponse = await fetchPosts(username, post_limit, "reels");
     // if(postFetchResponse.status != 200){
     //     logger.info(`🔴 Error in fetching reels for ${username}, only ${reelFetchResponse.data.length} posts fetched`);
-    //     return { status: 500, success: false}; 
+    //     return { status: 500, success: false};
     // }
     // const reelData = reelFetchResponse.data;
 
     // Save the posts to the database
-    const saveResponse = await savePosts(postData, user_id, task_id, followers_count);
-    if(saveResponse.status != 200){
-        return { status: 500, success: false, message: "Error in saving posts"}
+    const saveResponse = await savePosts(
+      postData,
+      user_id,
+      task_id,
+      followers_count
+    );
+    if (saveResponse.status != 200) {
+      return { status: 500, success: false, message: "Error in saving posts" };
     }
     return { success: true, total_posts: 1 };
   } catch (error) {
